965cea007ce53394908970ef48746e69f477afe9
Shivani Shriramojwar <shivani@Shivanis-MacBook-Air.local>
2026-05-29 21:10:02 +0530

    url changes in mongo .ts file

--- src/infrastructure/db/mongo.ts
+++ src/infrastructure/db/mongo.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in environment variables");
+    if (!process.env.MONGO_URL) {
+      throw new Error("MONGO_URL is not defined in environment variables");
     }
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB connected");
   } catch (error) {
